afe63ee2d1d161801c7ab0fcee497ccd2a5cb7fb
bullet3113 <0hitesh.a@gmail.com>
2023-11-02 09:32:27 +0530

    how create element works

--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -2,8 +2,19 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
+const obj = (
+  <a href="google.com" target="_blank"> visit google </a>
+);
+
+const reactElement = React.createElement(
+  'a',
+  {
+    'href': "google.com",
+    'target': "_blank"
+  },
+  'visit google'
+);
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    reactElement
 )
